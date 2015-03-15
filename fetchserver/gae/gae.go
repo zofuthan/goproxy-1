28e9c904759ba3f5e4936e5ed9bb71b87b0dcaea
Phus Lu <phuslu@hotmail.com>
2015-03-16 00:30:25 +0800

    refine gae.go

--- fetchserver/gae/gae.go
+++ fetchserver/gae/gae.go
@@ -4,13 +4,10 @@ package gae
 
 import (
 	"bytes"
-	"compress/zlib"
-	"encoding/binary"
-	"encoding/hex"
+	"compress/gzip"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
-	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -23,224 +20,105 @@ const (
 	Version  = "1.0"
 	Password = ""
 
-	FetchMax           = 3
 	FetchMaxSize       = 1024 * 1024 * 4
 	Deadline     int64 = 30 * 1e9
 )
 
-func encodeData(dic map[string]string) []byte {
-	w := bytes.NewBufferString("")
-	for k, v := range dic {
-		if len(v) != 0 {
-			fmt.Fprintf(w, "&%s=%s", k, hex.EncodeToString([]byte(v)))
-		}
-	}
-	return w.Bytes()[1:]
+func favicon(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
 }
 
-func decodeData(qs []byte) map[string]string {
-	m := make(map[string]string)
-	for _, kv := range strings.Split(string(qs), "&") {
-		if kv != "" {
-			pair := strings.Split(kv, "=")
-			value, _ := hex.DecodeString(pair[1])
-			m[pair[0]] = string(value)
-		}
-	}
-	return m
+func robots(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("User-agent: *\nDisallow: /\n"))
 }
 
-type Handler struct {
-	response http.ResponseWriter
-	request  *http.Request
-	context  appengine.Context
-	http.Handler
-}
+func handler(w http.ResponseWriter, r *http.Request) {
+	context := appengine.NewContext(r)
 
-func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	h.response = w
-	h.request = r
-	h.context = appengine.NewContext(r)
-	if r.Method == "POST" {
-		h.post()
-	} else {
-		h.get()
-	}
-}
-
-func (h Handler) printResponse(status int, header map[string]string, content []byte) {
-	headerEncoded := encodeData(header)
-
-	h.response.WriteHeader(200)
-	h.response.Header().Set("Content-Type", "image/gif")
-
-	compressed := false
-	if contentType, ok := header["content-type"]; ok {
-		if strings.HasPrefix(contentType, "text/") || strings.HasPrefix(contentType, "application/json") || strings.HasPrefix(contentType, "application/javascript") {
-			compressed = true
-		}
-	}
-
-	if compressed {
-		h.response.Write([]byte("1"))
-		w, err := zlib.NewWriterLevel(h.response, zlib.BestCompression)
+	if "gzip" == r.Header.Get("Content-Encoding") {
+		b, err := gzip.NewReader(r.Body)
+		r.Body = b
 		if err != nil {
-			h.context.Criticalf("zlib.NewWriterLevel(h.response, zlib.BestCompression) Error: %v", err)
-			return
+			context.Criticalf("gzip.NewReader(%#v) return %#v", r.Body, err)
 		}
-		defer w.Close()
-		binary.Write(w, binary.BigEndian, uint32(status))
-		binary.Write(w, binary.BigEndian, uint32(len(headerEncoded)))
-		binary.Write(w, binary.BigEndian, uint32(len(content)))
-		w.Write(headerEncoded)
-		w.Write(content)
-	} else {
-		h.response.Write([]byte("0"))
-		binary.Write(h.response, binary.BigEndian, uint32(status))
-		binary.Write(h.response, binary.BigEndian, uint32(len(headerEncoded)))
-		binary.Write(h.response, binary.BigEndian, uint32(len(content)))
-		h.response.Write(headerEncoded)
-		h.response.Write(content)
 	}
-}
 
-func (h Handler) printNotify(method string, url string, status int, text string) {
-	content := []byte(fmt.Sprintf("<h2>Go Server Fetch Info</h2><hr noshade='noshade'><p>%s '%s'</p><p>Return Code: %d</p><p>Message: %s</p>", method, url, status, text))
-	headers := map[string]string{"content-type": "text/html"}
-	h.printResponse(status, headers, content)
-}
-
-func (h Handler) post() {
-	r, err := zlib.NewReader(h.request.Body)
+	req, err := http.ReadRequest(r.Body)
 	if err != nil {
-		h.context.Criticalf("zlib.NewReader(h.request.Body) Error: %v", err)
-		return
-	}
-	defer r.Close()
-	data, err := ioutil.ReadAll(r)
-	if err != nil {
-		h.context.Criticalf("ioutil.ReadAll(r) Error: %v", err)
-		return
-	}
-	request := decodeData(data)
-
-	method := request["method"]
-	url := request["url"]
-	headers := request["headers"]
-
-	if Password != "" {
-		password, ok := request["password"]
-		if !ok || password != Password {
-			h.printNotify(method, url, 403, " Wrong Password.")
-		}
+		context.Criticalf("http.ReadRequest(%#v) return %#v", r.Body, err)
 	}
 
-	if !strings.HasPrefix(url, "http") {
-		h.printNotify(method, url, 501, "Unsupported Scheme")
+	password := r.Header.Get("X-Fetch-Password")
+	if password != "" {
+		r.Header.Del(key)
 	}
-
-	payload := request["payload"]
-	req, err := http.NewRequest(method, url, bytes.NewBufferString(payload))
-	if err != nil {
-		h.printNotify(method, url, 500, "http.NewRequest(method, url, payload) failed")
-	}
-
-	for _, line := range strings.Split(headers, "\r\n") {
-		kv := strings.SplitN(line, ":", 2)
-		if len(kv) == 2 {
-			req.Header.Set(strings.Title(kv[0]), strings.TrimSpace(kv[1]))
-		}
-	}
-
-	fetchmax := FetchMax
-	if fetchmaxString, ok := request["fetchmax"]; ok {
-		fetchmax, err = strconv.Atoi(fetchmaxString)
-		if err != nil {
-			h.context.Errorf("strconv.Atoi(fetchmaxString=%v) error=%v", fetchmaxString, err)
-			fetchmax = FetchMax
-		}
+	if Password != "" && Password != password {
+		http.Error(w, "Wrong Password.", 403)
 	}
 
 	deadline := time.Duration(Deadline)
 
 	var errors []string
-	for i := 0; i < fetchmax; i++ {
-		t := &urlfetch.Transport{Context: h.context, Deadline: deadline, AllowInvalidServerCertificate: true}
+	for i := 0; i < 2; i++ {
+		t := &urlfetch.Transport{Context: context, Deadline: deadline, AllowInvalidServerCertificate: true}
 		resp, err := t.RoundTrip(req)
 		if err != nil {
-			message := err.Error()
+			message := err.String()
 			errors = append(errors, message)
 			if strings.Contains(message, "FETCH_ERROR") {
-				h.context.Errorf("URLFetchServiceError_FETCH_ERROR(type=%T, deadline=%v, url=%v)", err, deadline, url)
-				time.Sleep(1 * 1e9)
+				context.Errorf("URLFetchServiceError_FETCH_ERROR(type=%T, deadline=%v, url=%v)", err, deadline, req.URL)
+				time.Sleep(time.Second)
 				deadline = time.Duration(Deadline * 2)
 			} else if strings.Contains(message, "DEADLINE_EXCEEDED") {
-				h.context.Errorf("URLFetchServiceError_DEADLINE_EXCEEDED(type=%T, deadline=%v, url=%v)", err, deadline, url)
-				time.Sleep(1 * 1e9)
+				context.Errorf("URLFetchServiceError_DEADLINE_EXCEEDED(type=%T, deadline=%v, url=%v)", err, deadline, req.URL)
+				time.Sleep(time.Second)
 				deadline = time.Duration(Deadline * 2)
 			} else if strings.Contains(message, "INVALID_URL") {
-				h.printNotify(method, url, 501, fmt.Sprintf("Invalid URL: %s", err.Error()))
+				http.Error(w, fmt.Sprintf("Invalid URL: %v", err), 501)
 				return
 			} else if strings.Contains(message, "RESPONSE_TOO_LARGE") {
-				h.context.Errorf("URLFetchServiceError_RESPONSE_TOO_LARGE(type=%T, deadline=%v, url=%v)", err, deadline, url)
+				context.Errorf("URLFetchServiceError_RESPONSE_TOO_LARGE(type=%T, deadline=%v, url=%v)", err, deadline, req.URL)
 				req.Header.Set("Range", fmt.Sprintf("bytes=0-%d", FetchMaxSize))
-				//h.context.Infof("req.Header=%v", req.Header)
 				deadline = time.Duration(Deadline * 2)
 			} else {
-				h.context.Errorf("URLFetchServiceError UNKOWN(type=%T, deadline=%v, url=%v, error=%v)", err, deadline, url, err)
-				time.Sleep(4 * 1e9)
+				context.Errorf("URLFetchServiceError UNKOWN(type=%T, deadline=%v, url=%v, error=%v)", err, deadline, req.URL, err)
+				time.Sleep(4 * time.Second)
 			}
 			continue
 		}
-
-		status := resp.StatusCode
-		header := make(map[string]string)
-		for k, vv := range resp.Header {
-			key := strings.ToLower(k)
-			if key != "set-cookie" {
-				header[key] = vv[0]
-			} else {
-				var cookies []string
-				i := -1
-				regex, _ := regexp.Compile("^[^ =]+ ")
-				for _, sc := range strings.Split(vv[0], ", ") {
-					if 0 <= i && regex.MatchString(sc) {
-						cookies[i] = fmt.Sprintf("%s, %s", cookies[i], sc)
-					} else {
-						cookies = append(cookies, sc)
-						i++
-					}
-				}
-				header["set-cookie"] = strings.Join(cookies, "\r\nSet-Cookie: ")
+		if resp.TransferEncoding != nil || resp.ContentLength >= 1024*1024 {
+			w.Header().Add("Content-Encoding", "gzip")
+			w, err = gzip.NewWriter(w)
+			if err != nil {
+				context.Criticalf("gzip.NewWriter(%#v) return %#v", w, err)
 			}
 		}
-
-		content, err := ioutil.ReadAll(resp.Body)
-		if err == urlfetch.ErrTruncatedBody {
-			h.context.Criticalf("ioutil.ReadAll(resp.Body) return urlfetch.ErrTruncatedBody")
+		fmt.Fprintf(w, "%s %s\r\n", resp.Proto, resp.Status)
+		for key, values := range resp.Header {
+			for _, value := range values {
+				fmt.Fprintf(w, "%s: %s\r\n", key, value)
+			}
 		}
-		if status == 206 {
-			header["accept-ranges"] = "bytes"
-			header["content-length"] = strconv.Itoa(len(content))
-		}
-		header["connection"] = "close"
-
-		//h.printNotify(method, url, 502, fmt.Sprintf("status=%d, header=%v, len(content)=%d", status, resp.Header, len(content)))
-		h.printResponse(status, header, content)
+		w.Write("\r\n")
+		io.Copy(w, resp.Body)
 		return
 	}
-	h.printNotify(method, url, 502, fmt.Sprintf("Go Server Fetch Failed: %v", errors))
+	http.Error(w, fmt.Sprintf("Go Server Fetch Failed: %v", errors), 502)
 }
 
-func (h Handler) get() {
-	version, _ := strconv.ParseInt(strings.Split(appengine.VersionID(h.context), ".")[1], 10, 64)
+func root(w http.ResponseWriter, r *http.Request) {
+	context := appengine.NewContext(r)
+	version, _ := strconv.ParseInt(strings.Split(appengine.VersionID(context), ".")[1], 10, 64)
 	ctime := time.Unix(version/(1<<28)+8*3600, 0).Format(time.RFC3339)
-
-	h.response.WriteHeader(http.StatusOK)
-	h.response.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(h.response, "GoAgent Go Server %s \xe5\xb7\xb2\xe7\xbb\x8f\xe5\x9c\xa8\xe5\xb7\xa5\xe4\xbd\x9c\xe4\xba\x86\xef\xbc\x8c\xe9\x83\xa8\xe7\xbd\xb2\xe6\x97\xb6\xe9\x97\xb4 %s\n", Version, ctime)
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "GoAgent go server %s works, deployed at %s\n", Version, ctime)
 }
 
 func init() {
-	http.Handle("/_gh/", Handler{})
+	http.HandleFunc("/favicon.ico", favicon)
+	http.HandleFunc("/robots.txt", robots)
+	http.HandleFunc("/_gh/", handler)
+	http.HandleFunc("/", root)
 }
