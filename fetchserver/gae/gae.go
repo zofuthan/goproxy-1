e4bbb2b75a3003e3f1bc3fbe77094d931776a655
Phus Lu <phuslu@hotmail.com>
2015-03-16 01:10:23 +0800

    refine httpError for gae.go

--- fetchserver/gae/gae.go
+++ fetchserver/gae/gae.go
@@ -3,7 +3,7 @@
 package gae
 
 import (
-	"bytes"
+	"bufio"
 	"compress/gzip"
 	"fmt"
 	"io"
@@ -20,8 +20,8 @@ const (
 	Version  = "1.0"
 	Password = ""
 
-	FetchMaxSize       = 1024 * 1024 * 4
-	Deadline     int64 = 30 * 1e9
+	FetchMaxSize = 1024 * 1024 * 4
+	Deadline     = 30 * time.Second
 )
 
 func favicon(w http.ResponseWriter, r *http.Request) {
@@ -33,78 +33,90 @@ func robots(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("User-agent: *\nDisallow: /\n"))
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	context := appengine.NewContext(r)
+func httpError(w http.ResponseWriter, html string, code int) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "HTTP/1.1 %d\r\n", code)
+	fmt.Fprintf(w, "Content-Type: text/html; charset=utf-8\r\n")
+	fmt.Fprintf(w, "Content-Length: %d\r\n", len(html))
+	w.Write([]byte("\r\n"))
+	w.Write([]byte(html))
+}
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	var err error
+	context := appengine.NewContext(r)
 	if "gzip" == r.Header.Get("Content-Encoding") {
-		b, err := gzip.NewReader(r.Body)
-		r.Body = b
+		r.Body, err = gzip.NewReader(r.Body)
 		if err != nil {
 			context.Criticalf("gzip.NewReader(%#v) return %#v", r.Body, err)
 		}
 	}
 
-	req, err := http.ReadRequest(r.Body)
+	req, err := http.ReadRequest(bufio.NewReader(r.Body))
 	if err != nil {
 		context.Criticalf("http.ReadRequest(%#v) return %#v", r.Body, err)
 	}
 
-	password := r.Header.Get("X-Fetch-Password")
-	if password != "" {
-		r.Header.Del(key)
+	params := make(map[string]string, 4)
+	paramPrefix := "X-Fetch-"
+	for key, values := range r.Header {
+		if strings.HasPrefix(key, paramPrefix) {
+			params[strings.ToLower(key[len(paramPrefix):])] = values[0]
+		}
+		req.Header.Del(key)
 	}
-	if Password != "" && Password != password {
-		http.Error(w, "Wrong Password.", 403)
+	if Password != "" {
+		if password, ok := params["password"]; !ok || password != Password {
+			httpError(w, "Wrong Password.", 403)
+		}
 	}
 
-	deadline := time.Duration(Deadline)
+	deadline := Deadline
 
 	var errors []string
 	for i := 0; i < 2; i++ {
 		t := &urlfetch.Transport{Context: context, Deadline: deadline, AllowInvalidServerCertificate: true}
 		resp, err := t.RoundTrip(req)
 		if err != nil {
-			message := err.String()
+			message := err.Error()
 			errors = append(errors, message)
 			if strings.Contains(message, "FETCH_ERROR") {
 				context.Errorf("URLFetchServiceError_FETCH_ERROR(type=%T, deadline=%v, url=%v)", err, deadline, req.URL)
 				time.Sleep(time.Second)
-				deadline = time.Duration(Deadline * 2)
+				deadline *= 2
 			} else if strings.Contains(message, "DEADLINE_EXCEEDED") {
 				context.Errorf("URLFetchServiceError_DEADLINE_EXCEEDED(type=%T, deadline=%v, url=%v)", err, deadline, req.URL)
 				time.Sleep(time.Second)
-				deadline = time.Duration(Deadline * 2)
+				deadline *= 2
 			} else if strings.Contains(message, "INVALID_URL") {
-				http.Error(w, fmt.Sprintf("Invalid URL: %v", err), 501)
+				httpError(w, fmt.Sprintf("Invalid URL: %v", err), 501)
 				return
 			} else if strings.Contains(message, "RESPONSE_TOO_LARGE") {
 				context.Errorf("URLFetchServiceError_RESPONSE_TOO_LARGE(type=%T, deadline=%v, url=%v)", err, deadline, req.URL)
 				req.Header.Set("Range", fmt.Sprintf("bytes=0-%d", FetchMaxSize))
-				deadline = time.Duration(Deadline * 2)
+				deadline *= 2
 			} else {
 				context.Errorf("URLFetchServiceError UNKOWN(type=%T, deadline=%v, url=%v, error=%v)", err, deadline, req.URL, err)
 				time.Sleep(4 * time.Second)
 			}
 			continue
 		}
+		var out io.Writer
 		if resp.TransferEncoding != nil || resp.ContentLength >= 1024*1024 {
 			w.Header().Add("Content-Encoding", "gzip")
-			w, err = gzip.NewWriter(w)
-			if err != nil {
-				context.Criticalf("gzip.NewWriter(%#v) return %#v", w, err)
-			}
+			out = gzip.NewWriter(w)
 		}
-		fmt.Fprintf(w, "%s %s\r\n", resp.Proto, resp.Status)
+		fmt.Fprintf(out, "%s %s\r\n", resp.Proto, resp.Status)
 		for key, values := range resp.Header {
 			for _, value := range values {
-				fmt.Fprintf(w, "%s: %s\r\n", key, value)
+				fmt.Fprintf(out, "%s: %s\r\n", key, value)
 			}
 		}
-		w.Write("\r\n")
-		io.Copy(w, resp.Body)
+		out.Write([]byte("\r\n"))
+		io.Copy(out, resp.Body)
 		return
 	}
-	http.Error(w, fmt.Sprintf("Go Server Fetch Failed: %v", errors), 502)
+	httpError(w, fmt.Sprintf("Go Server Fetch Failed: %v", errors), 502)
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
