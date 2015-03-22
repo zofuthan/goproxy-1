6ead6efa929d4db732246d20a552cd66c124b976
Phus Lu <phuslu@hotmail.com>
2015-03-22 22:50:59 +0800

    use internal ini config parser

--- goagent/config.go
+++ goagent/config.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/dlintw/goconf"
+	"io/ioutil"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -73,9 +74,9 @@ type CommonConfig struct {
 	LoveEnable          bool
 	LoveTip             []string
 
-	IplistCNames      map[string][]string
+	IplistMap         map[string][]string
 	IplistFixed       []string
-	IpListMap         map[string]string
+	HostMap           map[string]string
 	UrlRewriteMap     map[string]string
 	CrlfSites         []string
 	NocrlfSites       []string
@@ -88,131 +89,167 @@ type CommonConfig struct {
 	WithVPSSites      []string
 }
 
-func getString(c *goconf.ConfigFile, section string, option string) string {
-	value, err := c.GetString(section, option)
-	if err != nil {
-		panic(err)
-	}
-	return value
-}
+type GoConfig map[string]map[string]string
 
-func getStrings(c *goconf.ConfigFile, section string, option string) []string {
-	value, err := c.GetString(section, option)
-	if err != nil {
-		panic(err)
-	}
-	return regexp.MustCompile(`[|,]`).Split(value, -1)
-}
+func ReadConfig(filename string) (GoConfig, error) {
+	config := GoConfig{}
 
-func getInt(c *goconf.ConfigFile, section string, option string) int {
-	value, err := c.GetInt(section, option)
-	if err != nil {
-		panic(err)
-	}
-	return value
-}
-
-func getBool(c *goconf.ConfigFile, section string, option string) bool {
-	value, err := c.GetBool(section, option)
-	if err != nil {
-		panic(err)
-	}
-	return value
-}
-
-func ReadConfigFile(filename string) (*CommonConfig, error) {
-	cc := &CommonConfig{}
-	c, err := goconf.ReadConfigFile(filename)
+	b, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return nil, err
 	}
 
-	cc.ListenIp = getString(c, "listen", "ip")
-	cc.ListenPort = getInt(c, "listen", "port")
-	cc.ListenUsername = getString(c, "listen", "username")
-	cc.ListenPassword = getString(c, "listen", "password")
-	cc.ListenVisible = getBool(c, "listen", "visible")
-	cc.ListenDebuginfo = getBool(c, "listen", "debuginfo")
-
-	cc.GaeEnable = getBool(c, "gae", "enable")
-	cc.GaeAppids = getStrings(c, "gae", "appid")
-	cc.GaePassword = getString(c, "gae", "password")
-	cc.GaePath = getString(c, "gae", "path")
-	cc.GaeMode = getString(c, "gae", "mode")
-	cc.GaeIpv6 = getBool(c, "gae", "ipv6")
-	cc.GaeSslversion = getString(c, "gae", "sslversion")
-	cc.GaeWindow = getInt(c, "gae", "window")
-	cc.GaeCachesock = getBool(c, "gae", "cachesock")
-	cc.GaeHeadfirst = getBool(c, "gae", "headfirst")
-	cc.GaeKeepalive = getBool(c, "gae", "keepalive")
-	cc.GaeObfuscate = getBool(c, "gae", "obfuscate")
-	cc.GaeValidate = getBool(c, "gae", "validate")
-	cc.GaeTransport = getBool(c, "gae", "transport")
-	cc.GaeOptions = getStrings(c, "gae", "options")
-	cc.GaeRegions = getStrings(c, "gae", "regions")
-
-	cc.PacEnable = getBool(c, "pac", "enable")
-	cc.PacIp = getString(c, "pac", "ip")
-	cc.PacPort = getInt(c, "pac", "port")
-	cc.PacFile = getString(c, "pac", "file")
-	cc.PacAdmode = getInt(c, "pac", "admode")
-	cc.PacAdblock = getString(c, "pac", "adblock")
-	cc.PacGfwlist = getString(c, "pac", "gfwlist")
-	cc.PacExpired = getInt(c, "pac", "expired")
-
-	cc.PhpEnable = getBool(c, "php", "enable")
-	cc.PhpListen = getString(c, "php", "listen")
-	cc.PhpPassword = getString(c, "php", "password")
-	cc.PhpCrlf = getBool(c, "php", "crlf")
-	cc.PhpValidate = getBool(c, "php", "validate")
-	cc.PhpKeepalive = getBool(c, "php", "keepalive")
-	cc.PhpFetchserver = getStrings(c, "php", "fetchserver")
-	cc.PhpHosts = getStrings(c, "php", "hosts")
-
-	cc.VpsEnable = getBool(c, "vps", "enable")
-	cc.VpsListen = getString(c, "vps", "listen")
-	cc.VpsFetchserver = getStrings(c, "vps", "fetchserver")
-
-	cc.ProxyEnable = getBool(c, "proxy", "enable")
-	cc.ProxyAutodetect = getBool(c, "proxy", "autodetect")
-	cc.ProxyHost = getString(c, "proxy", "host")
-	cc.ProxyPort = getInt(c, "proxy", "port")
-	cc.ProxyUsername = getString(c, "proxy", "username")
-	cc.ProxyPasswrod = getString(c, "proxy", "password")
-
-	cc.AutorangeHosts = getStrings(c, "autorange", "hosts")
-	cc.AutorangeEndswith = getStrings(c, "autorange", "endswith")
-	cc.AutorangeNoendswith = getStrings(c, "autorange", "noendswith")
-	cc.AutorangeThreads = getInt(c, "autorange", "threads")
-	cc.AutorangeMaxsize = getInt(c, "autorange", "maxsize")
-	cc.AutorangeWaitsize = getInt(c, "autorange", "waitsize")
-	cc.AutorangeBufsize = getInt(c, "autorange", "bufsize")
-
-	cc.DnsEnable = getBool(c, "dns", "enable")
-	cc.DnsListen = getString(c, "dns", "listen")
-	cc.DnsServers = getStrings(c, "dns", "servers")
-
-	cc.UseragentEnable = getBool(c, "useragent", "enable")
-	cc.UseragentString = getString(c, "useragent", "string")
-
-	cc.FetchmaxLocal = getInt(c, "fetchmax", "local")
-	cc.FetchmaxServer = getInt(c, "fetchmax", "server")
-
-	cc.LoveEnable = getBool(c, "love", "enable")
-	cc.LoveTip = getStrings(c, "love", "tip")
-
-	cc.IplistCNames = make(map[string][]string)
-	if options, err := c.GetOptions("iplist"); err == nil {
-		for _, option := range options {
-			cc.IplistCNames[option] = getStrings(c, "iplist", option)
+	lines := strings.Split(strings.Replace(string(b), "\r\n", "\n", -1), "\n")
+	section := "default"
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if line == "" || line[0] == ';' {
+			continue
 		}
-	} else {
+		if line[0] == '[' && line[len(line)-1] == ']' {
+			section = line[1 : len(line)-1]
+			config[section] = make(map[string]string, 0)
+		} else if strings.Contains(line, " =") {
+			items := strings.SplitN(line, " =", 2)
+			if _, ok := config[section]; !ok {
+				config[section] = make(map[string]string, 0)
+			}
+			config[section][strings.TrimSpace(items[0])] = strings.TrimSpace(items[1])
+		}
+	}
+	return config, nil
+}
+
+func (c GoConfig) GetOptions(section string) []string {
+	options := make([]string, 0)
+	if _, ok := c[section]; !ok {
+		panic(fmt.Errorf("section %#v not exists", section))
+	}
+	for option, _ := range c[section] {
+		options = append(options, option)
+	}
+	return options
+}
+
+func (c GoConfig) GetString(section string, option string) string {
+	if _, ok := c[section]; !ok {
+		panic(fmt.Errorf("section %#v not exists", section))
+	}
+	if _, ok := c[section][option]; !ok {
+		panic(fmt.Errorf("section %#v option %#v not exists", section, option))
+	}
+	return c[section][option]
+}
+
+func (c GoConfig) GetStrings(section string, option string) []string {
+	return regexp.MustCompile(`[|,]`).Split(c.GetString(section, option), -1)
+}
+
+func (c GoConfig) GetInt(section string, option string) int {
+	value, err := strconv.Atoi(c.GetString(section, option))
+	if err != nil {
 		panic(err)
 	}
+	return value
+}
 
-	cc.IplistCNames = make(map[string][]string, 0)
+func (c GoConfig) GetBool(section string, option string) bool {
+	switch strings.ToLower(c.GetString(section, option)) {
+	case "1":
+		return true
+	case "true":
+		return true
+	case "t":
+		return true
+	default:
+		return false
+	}
+}
+
+func ReadConfigFile(filename string) (*CommonConfig, error) {
+	c, err := ReadConfig(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	cc := &CommonConfig{}
+	cc.ListenIp = c.GetString("listen", "ip")
+	cc.ListenPort = c.GetInt("listen", "port")
+	cc.ListenUsername = c.GetString("listen", "username")
+	cc.ListenPassword = c.GetString("listen", "password")
+	cc.ListenVisible = c.GetBool("listen", "visible")
+	cc.ListenDebuginfo = c.GetBool("listen", "debuginfo")
+
+	cc.GaeEnable = c.GetBool("gae", "enable")
+	cc.GaeAppids = c.GetStrings("gae", "appid")
+	cc.GaePassword = c.GetString("gae", "password")
+	cc.GaePath = c.GetString("gae", "path")
+	cc.GaeMode = c.GetString("gae", "mode")
+	cc.GaeIpv6 = c.GetBool("gae", "ipv6")
+	cc.GaeSslversion = c.GetString("gae", "sslversion")
+	cc.GaeWindow = c.GetInt("gae", "window")
+	cc.GaeCachesock = c.GetBool("gae", "cachesock")
+	cc.GaeHeadfirst = c.GetBool("gae", "headfirst")
+	cc.GaeKeepalive = c.GetBool("gae", "keepalive")
+	cc.GaeObfuscate = c.GetBool("gae", "obfuscate")
+	cc.GaeValidate = c.GetBool("gae", "validate")
+	cc.GaeTransport = c.GetBool("gae", "transport")
+	cc.GaeOptions = c.GetStrings("gae", "options")
+	cc.GaeRegions = c.GetStrings("gae", "regions")
+
+	cc.PacEnable = c.GetBool("pac", "enable")
+	cc.PacIp = c.GetString("pac", "ip")
+	cc.PacPort = c.GetInt("pac", "port")
+	cc.PacFile = c.GetString("pac", "file")
+	cc.PacAdmode = c.GetInt("pac", "admode")
+	cc.PacAdblock = c.GetString("pac", "adblock")
+	cc.PacGfwlist = c.GetString("pac", "gfwlist")
+	cc.PacExpired = c.GetInt("pac", "expired")
+
+	cc.PhpEnable = c.GetBool("php", "enable")
+	cc.PhpListen = c.GetString("php", "listen")
+	cc.PhpPassword = c.GetString("php", "password")
+	cc.PhpCrlf = c.GetBool("php", "crlf")
+	cc.PhpValidate = c.GetBool("php", "validate")
+	cc.PhpKeepalive = c.GetBool("php", "keepalive")
+	cc.PhpFetchserver = c.GetStrings("php", "fetchserver")
+	cc.PhpHosts = c.GetStrings("php", "hosts")
+
+	cc.VpsEnable = c.GetBool("vps", "enable")
+	cc.VpsListen = c.GetString("vps", "listen")
+	cc.VpsFetchserver = c.GetStrings("vps", "fetchserver")
+
+	cc.ProxyEnable = c.GetBool("proxy", "enable")
+	cc.ProxyAutodetect = c.GetBool("proxy", "autodetect")
+	cc.ProxyHost = c.GetString("proxy", "host")
+	cc.ProxyPort = c.GetInt("proxy", "port")
+	cc.ProxyUsername = c.GetString("proxy", "username")
+	cc.ProxyPasswrod = c.GetString("proxy", "password")
+
+	cc.AutorangeHosts = c.GetStrings("autorange", "hosts")
+	cc.AutorangeEndswith = c.GetStrings("autorange", "endswith")
+	cc.AutorangeNoendswith = c.GetStrings("autorange", "noendswith")
+	cc.AutorangeThreads = c.GetInt("autorange", "threads")
+	cc.AutorangeMaxsize = c.GetInt("autorange", "maxsize")
+	cc.AutorangeWaitsize = c.GetInt("autorange", "waitsize")
+	cc.AutorangeBufsize = c.GetInt("autorange", "bufsize")
+
+	cc.DnsEnable = c.GetBool("dns", "enable")
+	cc.DnsListen = c.GetString("dns", "listen")
+	cc.DnsServers = c.GetStrings("dns", "servers")
+
+	cc.UseragentEnable = c.GetBool("useragent", "enable")
+	cc.UseragentString = c.GetString("useragent", "string")
+
+	cc.FetchmaxLocal = c.GetInt("fetchmax", "local")
+	cc.FetchmaxServer = c.GetInt("fetchmax", "server")
+
+	cc.LoveEnable = c.GetBool("love", "enable")
+	cc.LoveTip = c.GetStrings("love", "tip")
+
+	cc.IplistMap = make(map[string][]string)
 	cc.IplistFixed = make([]string, 0)
-	cc.IpListMap = make(map[string]string, 0)
+	cc.HostMap = make(map[string]string, 0)
 	cc.UrlRewriteMap = make(map[string]string, 0)
 	cc.CrlfSites = make([]string, 0)
 	cc.NocrlfSites = make([]string, 0)
@@ -223,50 +260,47 @@ func ReadConfigFile(filename string) (*CommonConfig, error) {
 	cc.WithGAESites = make([]string, 0)
 	cc.WithPHPSites = make([]string, 0)
 	cc.WithVPSSites = make([]string, 0)
-	if options, err := c.GetOptions("profile"); err == nil {
-		for _, option := range options {
-			pattern := option
-			rules := getStrings(c, "profile", option)
-			for {
-				if len(rules) == 0 {
-					break
-				}
-				rule := rules[0]
-				rules = rules[1:]
-				switch rule {
-				case "crlf":
-					cc.CrlfSites = append(cc.CrlfSites, pattern)
-				case "nocrlf":
-					cc.NocrlfSites = append(cc.NocrlfSites, pattern)
-				case "forcehttps":
-					cc.ForcehttpsSites = append(cc.ForcehttpsSites, pattern)
-				case "noforcehttps":
-					cc.NoforcehttpsSites = append(cc.NoforcehttpsSites, pattern)
-				case "fakehttps":
-					cc.FakehttpsSites = append(cc.FakehttpsSites, pattern)
-				case "nofakehttps":
-					cc.NofakehttpsSites = append(cc.NofakehttpsSites, pattern)
-				case "withgae":
-					cc.WithGAESites = append(cc.WithGAESites, pattern)
-				case "withphp":
-					cc.WithPHPSites = append(cc.WithPHPSites, pattern)
-				case "withvps":
-					cc.WithVPSSites = append(cc.WithVPSSites, pattern)
-				case "direct":
-					cc.IpListMap[pattern] = pattern
-					cc.IplistCNames[pattern] = nil
-				default:
-					fmt.Printf("rule=%#v, pattern=%#v\n", rule, pattern)
-					if _, ok := cc.IplistCNames[rule]; ok {
-						cc.IpListMap[pattern] = rule
-					} else if strings.Contains(pattern, "\\") {
-						cc.UrlRewriteMap[pattern] = rule
-					}
+	for _, option := range c.GetOptions("iplist") {
+		cc.IplistMap[option] = c.GetStrings("iplist", option)
+	}
+	for _, option := range c.GetOptions("profile") {
+		pattern := option
+		rules := c.GetStrings("profile", option)
+		for {
+			if len(rules) == 0 {
+				break
+			}
+			rule := rules[0]
+			rules = rules[1:]
+			switch rule {
+			case "crlf":
+				cc.CrlfSites = append(cc.CrlfSites, pattern)
+			case "nocrlf":
+				cc.NocrlfSites = append(cc.NocrlfSites, pattern)
+			case "forcehttps":
+				cc.ForcehttpsSites = append(cc.ForcehttpsSites, pattern)
+			case "noforcehttps":
+				cc.NoforcehttpsSites = append(cc.NoforcehttpsSites, pattern)
+			case "fakehttps":
+				cc.FakehttpsSites = append(cc.FakehttpsSites, pattern)
+			case "nofakehttps":
+				cc.NofakehttpsSites = append(cc.NofakehttpsSites, pattern)
+			case "withgae":
+				cc.WithGAESites = append(cc.WithGAESites, pattern)
+			case "withphp":
+				cc.WithPHPSites = append(cc.WithPHPSites, pattern)
+			case "withvps":
+				cc.WithVPSSites = append(cc.WithVPSSites, pattern)
+			case "direct":
+				cc.HostMap[pattern] = ""
+			default:
+				if _, ok := cc.IplistMap[rule]; ok {
+					cc.HostMap[pattern] = rule
+				} else if strings.Contains(pattern, "\\") {
+					cc.UrlRewriteMap[pattern] = rule
 				}
 			}
 		}
-	} else {
-		panic(err)
 	}
 
 	return cc, nil
