b1d1ab7268391c604110f826b02b2ae1ec79bdca
Phus Lu <phuslu@hotmail.com>
2015-03-23 23:17:30 +0800

    add MockRequestFilter

--- httpproxy/mock.go
+++ httpproxy/mock.go
@@ -1,19 +1,33 @@
 package httpproxy
 
 import (
-// "fmt"
-// "github.com/golang/glog"
-// "net/http"
+	"bytes"
+	"io/ioutil"
+	"net/http"
 )
 
 type MockRequestFilter struct {
-	RequestFilter
 }
 
-// func (f *MockRequestFilter) HandleRequest(h *Handler, args *FilterArgs, rw http.ResponseWriter, req *http.Request) (*http.Response, error) {
-// 	status := args.Get("status")
-// }
-
-// func (f *MockRequestFilter) Filter(req *http.Request) (args *FilterArgs, err error) {
-// 	return &FilterArgs{}, nil
-// }
+func (f *MockRequestFilter) HandleRequest(h *Handler, args *FilterArgs, rw http.ResponseWriter, req *http.Request) (*http.Response, error) {
+	status, err := args.GetString("status")
+	if err != nil {
+		return nil, err
+	}
+	header, err := args.GetHeader("header")
+	if err != nil {
+		return nil, err
+	}
+	content, err := args.GetString("content")
+	if err != nil {
+		return nil, err
+	}
+	return &http.Response{
+		Status:        status,
+		Header:        *header,
+		Body:          ioutil.NopCloser(bytes.NewBufferString(content)),
+		ContentLength: int64(len(content)),
+		Request:       req,
+		Close:         true,
+	}, nil
+}
